2b71e6d531714eb79789fdb74a52c7e592d8700f
deeshana23 <weerasekaradeeshana@gmail.com>
2026-09-19 00:14:03 +0530

    Add specialty lookup data

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
+#define SPECIALTY_COUNT 4
+
+const char *specialtyNames[SPECIALTY_COUNT] = {
+    "General Practice (OPD)",
+    "Paediatrics",
+    "Cardiology",
+    "Neurology"
+};
+
+const int specialtyFees[SPECIALTY_COUNT] = {1500, 2500, 4500, 5000};
+
+const int specialtyTimes[SPECIALTY_COUNT] = {15, 20, 30, 30};
+
+const int specialtyDailyCaps[SPECIALTY_COUNT] = { 30, 20, 12, 10};
+
 
 int main()
 {
